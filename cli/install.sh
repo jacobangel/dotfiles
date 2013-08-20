3f8000eb00e89226293f328c422619ccdc9d97c8
Jacob Angel <jacob.angel@gmail.com>
2013-08-20 15:09:17 -0400

    Add grabbing the completion bash for future.

--- cli/install.sh
+++ cli/install.sh
@@ -2,4 +2,10 @@
 
 ## Add to profile
 
+# add git completion to profile
+curl https://raw.github.com/git/git/master/contrib/completion/git-completion.bash -o ~/.dotfiles/cli/.git-completion.bash
+
+# add dotfile refernce
+# TODO add multiple tests for environments so that mac / linux / windows works.
 echo ". ~/.dotfiles/cli/.profile" >> ~/.profile
+
